4c7bd165ebc152c37c9d0a9c6e456a140031380b
Константин Абашин <kabashin@mail.ru>
2022-06-09 16:41:00 +0300

    Sprint 9 RC v2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,10 +2,7 @@ from django.core.exceptions import PermissionDenied
 from rest_framework import viewsets, filters, mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import (
-    IsAuthenticated,
-    IsAuthenticatedOrReadOnly
-)
+from rest_framework.permissions import IsAuthenticated
 
 from .permissions import AuthorOrReadOnly
 from .serializers import (GroupSerializer, PostSerializer, CommentSerializer,
